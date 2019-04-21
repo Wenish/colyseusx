b85dc22aca5ec44374ddee9cb11f1e38bf440d05
Wenish <wenish@noreply.github.com>
2019-04-21 14:30:37 +0200

    update

--- src/store.ts
+++ src/store.ts
@@ -2,6 +2,20 @@ export class Store<S> {
     constructor (options: StoreOptions<S> = {}) {
 
     }
+    readonly state: S;
+    readonly getters: any;
+
+    replaceState(state: S): void {
+        state = state
+    };
+
+    dispatch (type: string, payload: any) {
+
+    }
+
+    commit (type: string, payload: any) {
+
+    }
 }
 
 interface StoreOptions<S> {
